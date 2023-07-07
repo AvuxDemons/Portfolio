9e0851af657255df52549fb20ccf1791d29a1449
AvuxDemons <avuxdemons@gmail.com>
2023-07-08 00:32:03 +0700

    [/] typo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Live2D from './components/Live2D';
 import Hero from './components/Hero';
 import About from './components/About';
 import Project from './components/Project';
-import Contact from './components/contact';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './assets/css/main.css';
 
